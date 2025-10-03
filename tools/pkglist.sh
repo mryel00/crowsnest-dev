ea79de54ca2aabdb1ffcf04c16320c59f0ed63a4
Patrick Gehrsitz <mryel00.github@gmail.com>
2025-10-03 22:17:51 +0200

    fix: remove tools/pkglist.sh from .gitignore

---
+++ tools/pkglist.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#### crowsnest - A webcam Service for multiple Cams and Stream Services.
+####
+#### Written by Patrick Gehrsitz aka mryel00 <mryel00.github@gmail.com>
+#### Copyright 2025 - till today
+#### https://github.com/mainsail-crew/crowsnest
+####
+#### This File is distributed under GPLv3
+####
+
+# shellcheck enable=require-variable-braces
+
+# Exit on errors
+set -Ee
+
+# Debug
+# set -x
+
+### Crowsnest Dependencies
+PKGLIST="git crudini curl python3 python3-venv"
