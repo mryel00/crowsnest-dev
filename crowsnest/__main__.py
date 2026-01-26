29d9737ed25d2163302ee76d1ecaf3c6d583e0f8
Patrick Gehrsitz <mryel00.github@gmail.com>
2026-01-26 01:17:12 +0100

    refactor: use asyncio.run
    Signed-off-by: Patrick Gehrsitz <mryel00.github@gmail.com>

--- crowsnest/__main__.py
+++ crowsnest/__main__.py
@@ -3,8 +3,4 @@ import asyncio
 from crowsnest.crowsnest import main
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(main())
-    finally:
-        loop.close()
+    asyncio.run(main())
